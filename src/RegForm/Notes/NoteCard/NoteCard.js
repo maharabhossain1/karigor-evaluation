95612f93bb41dc4665589e461411a17f39c047f0
maharabhossain1 <maharabhossain0022@gmail.com>
2021-12-29 19:15:48 +0600

    cart style done

--- src/RegForm/Notes/NoteCard/NoteCard.js
+++ src/RegForm/Notes/NoteCard/NoteCard.js
@@ -1,17 +1,27 @@
 import { Typography } from "@mui/material";
 import React from "react";
 import "./NoteCard.css";
+import { CgArrowLongRight } from "react-icons/cg";
+import { FiUser } from "react-icons/fi";
+import { AiOutlineClockCircle } from "react-icons/ai";
+
 export default function NoteCard({ note }) {
-  const { studentId, studentName, date, notes } = note;
+  const { studentName, date, notes } = note;
   return (
     <div className="note-container">
       <div className="note-info">
-        <div>{studentName}</div>
-        <div>{date}</div>
+        <div>
+          <FiUser /> {studentName}
+        </div>
+        <div>
+          <AiOutlineClockCircle /> {date}
+        </div>
       </div>
       <div className="note-text">{notes}</div>
       <div className="detail-note">
-        <Typography>বিস্তারিত</Typography>
+        <Typography>
+          বিস্তারিত <CgArrowLongRight />
+        </Typography>
       </div>
     </div>
   );
