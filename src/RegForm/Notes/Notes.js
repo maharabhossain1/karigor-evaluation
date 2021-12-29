71e56477ee18a8f186829f494f1e6b4059803bb3
maharabhossain1 <maharabhossain0022@gmail.com>
2021-12-29 19:26:21 +0600

    step 3 done

--- src/RegForm/Notes/Notes.js
+++ src/RegForm/Notes/Notes.js
@@ -1,19 +1,31 @@
-import { Box } from "@mui/system";
-import React, { useEffect, useState } from "react";
+import { Grid, Button } from "@mui/material";
+import Box from "@mui/material/Box";
+import React, { useContext, useEffect, useState } from "react";
 import NoteCard from "./NoteCard/NoteCard";
+import { multiStepContext } from "../../context/StepContext";
 
 export default function Notes() {
   const [notesData, setNotesData] = useState([]);
+  const { setCourrentStep } = useContext(multiStepContext);
   useEffect(() => {
     fetch("/data.json")
       .then((res) => res.json())
       .then((data) => setNotesData(data));
   }, []);
   return (
-    <Box>
-      {notesData.map((note) => (
-        <NoteCard key={note.studentId} note={note} />
-      ))}
+    <Box sx={{ flexGrow: 1, my: 5 }}>
+      <Grid container spacing={2}>
+        {notesData.map((note) => (
+          <Grid item xs={8} md={3}>
+            <NoteCard key={note.studentId} note={note} />
+          </Grid>
+        ))}
+      </Grid>
+      <Box>
+        <Button onClick={() => setCourrentStep(4)} variant="contained">
+          Next
+        </Button>
+      </Box>
     </Box>
   );
 }
