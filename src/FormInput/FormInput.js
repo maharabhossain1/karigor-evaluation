22326c9d12ffa3ff14f25325849f24bac32c8a87
maharabhossain1 <maharabhossain0022@gmail.com>
2022-01-03 11:46:06 +0600

    pic consider implimentation

--- src/FormInput/FormInput.js
+++ src/FormInput/FormInput.js
@@ -5,8 +5,15 @@ export default function FormInput(props) {
   const { label, handleOnBlur, value, ...others } = props;
   return (
     <div className="input-field">
-      <label htmlFor="text">{label}</label>
-      <input onBlur={handleOnBlur} defaultValue={value || ""} {...others} />
+      {others.type === "file" ? (
+        // for farther implimentation
+        <div></div>
+      ) : (
+        <div>
+          <label htmlFor="text">{label}</label>
+          <input onBlur={handleOnBlur} defaultValue={value || ""} {...others} />
+        </div>
+      )}
     </div>
   );
 }
