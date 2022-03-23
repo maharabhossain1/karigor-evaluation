7db8558d8a5274802d3169ba5550a0b2634c97fc
maharabhossain1 <maharabhossain0022@gmail.com>
2022-03-23 17:48:16 +0600

    enzyme install and setup

---
+++ src/Components/CommentApp/__tests__/index.test.js
@@ -0,0 +1,11 @@
+import React from "react";
+import reactDom from "react-dom";
+import CommentApp from "../index";
+
+it("show a comment box", () => {
+  // const div = document.createElement("div");
+  // reactDom.render(<CommentApp />, div);
+  // //Look inside the div and check to see if the comment box is in there
+  // reactDom.unmountComponentAtNode(div);
+  // const wrapped = shallow(<CommentApp />);
+});
